09afac5f71c76c444a8c73536aec42b104afac65
Peter <peter@armnova>
2023-07-30 16:43:04 -0400

    test scene

--- include/mjregrasping/planning.h
+++ include/mjregrasping/planning.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_pipeline/planning_pipeline.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit_msgs/PlanningScene.h>
 #include <moveit_msgs/RobotTrajectory.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <ros/ros.h>
 
 #include <arc_utilities/eigen_typedefs.hpp>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
@@ -13,8 +20,12 @@ class RRTPlanner {
  public:
   RRTPlanner();
 
-  std::pair<bool, moveit_msgs::RobotTrajectory> plan_from_q_to_target_pos(std::vector<double> const &q0,
-                                                                          VectorVector3d const &candidate_pos,
-                                                                          std::vector<std::string> const &strategy_strs,
-                                                                          std::vector<bool> const &is_grasping0);
+  moveit_msgs::MotionPlanResponse plan(moveit_msgs::PlanningScene const &scene_msg, std::string const &group_name,
+                                       std::map<std::string, Eigen::Vector3d> const &goal_positions);
+
+  ros::NodeHandle nh;
+  std::shared_ptr<robot_model_loader::RobotModelLoader> robot_model_loader;
+  std::shared_ptr<robot_model::RobotModel> robot_model;
+  std::shared_ptr<planning_pipeline::PlanningPipeline> planning_pipeline;
+  std::shared_ptr<moveit_visual_tools::MoveItVisualTools> visual_tools;
 };
--- src/planning.cpp
+++ src/planning.cpp
@@ -1,74 +1,54 @@
 #include <bio_ik/bio_ik.h>
 #include <mjregrasping/planning.h>
 #include <moveit/kinematic_constraints/utils.h>
-#include <moveit/planning_interface/planning_interface.h>
-#include <moveit/planning_pipeline/planning_pipeline.h>
-#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/conversions.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_visual_tools/moveit_visual_tools.h>
-#include <ros/ros.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-//    std::vector<double> out;
-//    }
-//    ROS_DEBUG_STREAM_NAMED("BIO_IK", "ok? " << ok);
-//    ROS_DEBUG_STREAM_NAMED("BIO_IK", "q " << out);
-//
-//    if (!ok) {
-//      return {};
-//    }
-//
-//    return {out};
-//  }
+constexpr auto LOGNAME = "planning";
 
-double deg2rad(double deg) { return deg * M_PI / 180.0; }
+namespace rvt = rviz_visual_tools;
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "planning_demo");
+constexpr double deg2rad(double deg) { return deg * M_PI / 180.0; }
+
+RRTPlanner::RRTPlanner() {
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  ros::NodeHandle nh;
 
-  std::string const group_name = "whole_body";
-
-  auto robot_model_loader = std::make_shared<robot_model_loader::RobotModelLoader>("hdt_michigan/robot_description");
-
-  auto robot_model = robot_model_loader->getModel();
+  robot_model_loader = std::make_shared<robot_model_loader::RobotModelLoader>("hdt_michigan/robot_description");
+  robot_model = robot_model_loader->getModel();
 
   nh.setParam("planning_plugin", "ompl_interface/OMPLPlanner");
 
-  auto planning_pipeline = std::make_shared<planning_pipeline::PlanningPipeline>(robot_model, nh);
+  planning_pipeline = std::make_shared<planning_pipeline::PlanningPipeline>(robot_model, nh);
 
-  auto display_publisher =
-      nh.advertise<moveit_msgs::DisplayTrajectory>("hdt_michigan/move_group/display_planned_path", 10);
-  moveit_msgs::DisplayTrajectory display_trajectory;
+  visual_tools = std::make_shared<moveit_visual_tools::MoveItVisualTools>("robot_root");
+  visual_tools->deleteAllMarkers();
+}
 
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("robot_root");
-  visual_tools.deleteAllMarkers();
-
+moveit_msgs::MotionPlanResponse RRTPlanner::plan(moveit_msgs::PlanningScene const &scene_msg,
+                                                 std::string const &group_name,
+                                                 std::map<std::string, Eigen::Vector3d> const &goal_positions) {
   auto planning_scene = std::make_shared<planning_scene::PlanningScene>(robot_model);
-  robot_state::RobotState state(robot_model);
-  // NOTE q could be passed in
-  std::vector<double> q(state.getVariableCount(), 0.0);
-  planning_scene->getCurrentStateNonConst().setVariablePositions(q);
+  planning_scene->setPlanningSceneMsg(scene_msg);
 
-  // Can we plan to keep one gripper still and move the other to a goal position?
+  auto state = planning_scene->getCurrentStateNonConst();
+
   state.update();  // update FK
-  auto const current_left_tool_tf = state.getGlobalLinkTransform("left_tool");
-  tf2::Vector3 const current_left_tool_position(current_left_tool_tf.translation().x(),
-                                                current_left_tool_tf.translation().y(),
-                                                current_left_tool_tf.translation().z());
 
   // Solve IK
   auto opts = std::make_unique<bio_ik::BioIKKinematicsQueryOptions>();
   opts->replace = true;                       // needed to replace the default goals!!!
   opts->return_approximate_solution = false;  // optional
-  opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>("left_tool", current_left_tool_position));
-  opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>("right_tool", tf2::Vector3(0.8, -0.2, 0.7)));
+  for (auto const &[name, p] : goal_positions) {
+    tf2::Vector3 position(p(0), p(1), p(2));
+    opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>(name, position));
+  }
 
   const moveit::core::JointModelGroup *jmg = state.getJointModelGroup(group_name);
 
+  // NOTE: could add collision checking to the IK callback here, but I don't think it's necessary because the RRT is
+  //     already doing collision checking.
   moveit::core::GroupStateValidityCallbackFn state_valid_cb =
       [](moveit::core::RobotState *robot_state, const moveit::core::JointModelGroup *joint_group,
          const double *joint_group_variable_values) { return true; };
@@ -76,7 +56,8 @@ int main(int argc, char **argv) {
   planning_interface::MotionPlanRequest req;
   req.group_name = group_name;
 
-  for (auto i{0}; i < 1; ++i) {
+  for (auto i{0}; i < 30; ++i) {
+    state.setToRandomPositionsNearBy(jmg, state, 0.25);
     auto const ok = state.setFromIK(jmg,                            // joints to be used for IK
                                     EigenSTL::vector_Isometry3d(),  // this isn't used, goals are described in opts
                                     std::vector<std::string>(),     // names of the end-effector links
@@ -86,7 +67,7 @@ int main(int argc, char **argv) {
       continue;
     }
 
-    visual_tools.publishRobotState(state, rvt::CYAN);
+    visual_tools->publishRobotState(state, rvt::CYAN);
 
     moveit_msgs::Constraints joints_goal_constraint;
     for (auto const &n : jmg->getActiveJointModelNames()) {
@@ -102,49 +83,75 @@ int main(int argc, char **argv) {
     req.goal_constraints.push_back(joints_goal_constraint);
   }
 
-  // keep the left tool where it currently is
-  moveit_msgs::PositionConstraint left_tool_path_constraint;
-  left_tool_path_constraint.link_name = "left_tool";
-  left_tool_path_constraint.header.frame_id = "robot_root";
-  left_tool_path_constraint.weight = 1.0;
-  // convert the Transform to a Pose message
-  geometry_msgs::Pose current_left_tool_pose;
-  // TODO: we must already have a function for this???
-  current_left_tool_pose.position.x = current_left_tool_tf.translation().x();
-  current_left_tool_pose.position.y = current_left_tool_tf.translation().y();
-  current_left_tool_pose.position.z = current_left_tool_tf.translation().z();
-  current_left_tool_pose.orientation.x = current_left_tool_tf.rotation()(0);
-  current_left_tool_pose.orientation.y = current_left_tool_tf.rotation()(1);
-  current_left_tool_pose.orientation.z = current_left_tool_tf.rotation()(2);
-  current_left_tool_pose.orientation.w = current_left_tool_tf.rotation()(3);
-  left_tool_path_constraint.constraint_region.primitive_poses.push_back(current_left_tool_pose);
-  shape_msgs::SolidPrimitive path_pos_constraint_region;
-  path_pos_constraint_region.type = path_pos_constraint_region.SPHERE;
-  path_pos_constraint_region.dimensions.push_back(1.0);
-  left_tool_path_constraint.constraint_region.primitives.push_back(path_pos_constraint_region);
-  req.path_constraints.position_constraints.push_back(left_tool_path_constraint);
-
-  ROS_INFO_STREAM("Planning with " << req.goal_constraints.size() << " goal constraints");
+  ROS_INFO_STREAM_NAMED(LOGNAME, "Planning with " << req.goal_constraints.size() << " goal constraints");
 
   planning_interface::MotionPlanResponse res;
   req.allowed_planning_time = 30.0;
   planning_pipeline->generatePlan(planning_scene, req, res);
 
-  if (res.error_code_.val != res.error_code_.SUCCESS) {
-    ROS_ERROR("Could not compute plan successfully");
-    return 0;
-  }
-
   moveit_msgs::MotionPlanResponse res_msg;
   res.getMessage(res_msg);
 
+  return res_msg;
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "planning_demo");
+
+  std::string const group_name = "right_arm";
+
+  auto planner = RRTPlanner();
+
+  robot_state::RobotState state(planner.robot_model);
+  // NOTE q could be passed in
+  std::vector<double> q(state.getVariableCount(), 0.0);
+
+  moveit_msgs::PlanningScene scene_msg;
+  scene_msg.is_diff = false;
+  scene_msg.robot_state.is_diff = false;
+  moveit::core::robotStateToRobotStateMsg(state, scene_msg.robot_state);
+  scene_msg.world.collision_objects.resize(1);
+  auto &co = scene_msg.world.collision_objects[0];
+  co.id = "box1";
+  co.header.frame_id = "robot_root";
+  co.pose.orientation.w = 1.0;
+  co.primitive_poses.resize(1);
+  auto &pose = co.primitive_poses[0];
+  pose.orientation.w = 1.0;
+  pose.position.x = 0.2;
+  pose.position.y = 0.7;
+  pose.position.z = 0.5;
+  co.primitives.resize(1);
+  auto &shape = co.primitives[0];
+  shape.dimensions.resize(3);
+  shape.dimensions[0] = 0.1;
+  shape.dimensions[1] = 0.2;
+  shape.dimensions[2] = 0.5;
+  shape.type = shape_msgs::SolidPrimitive::BOX;
+
+  auto scene_pub = planner.nh.advertise<moveit_msgs::PlanningScene>("scene_viz", 10);
+
+  for (auto i = 0; i < 10; ++i) {
+    scene_pub.publish(scene_msg);
+    usleep(100000);
+  }
+
+  return 0;
+
+  std::map<std::string, Eigen::Vector3d> goal_positions{
+      //      {"left_tool", Eigen::Vector3d(-0.2, 0.8, 0.7)},
+      {"right_tool", Eigen::Vector3d(0.2, 0.8, 0.7)},
+  };
+
+  auto const res_msg = planner.plan(scene_msg, group_name, goal_positions);
+
   moveit_msgs::DisplayTrajectory display_traj_msg;
-  display_traj_msg.model_id = robot_model->getName();
+  display_traj_msg.model_id = planner.robot_model->getName();
   display_traj_msg.trajectory_start = res_msg.trajectory_start;
   display_traj_msg.trajectory.push_back(res_msg.trajectory);
 
   for (auto i = 0; i < 10; ++i) {
-    display_publisher.publish(display_traj_msg);
+    planner.visual_tools->publishTrajectoryPath(display_traj_msg);
     usleep(100000);
   }
 
