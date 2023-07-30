93147c4a5b552bc5553aef09b6736a2deadc2be7
Peter <peter@armnova>
2023-07-30 15:31:10 -0400

    nope, path constrints not gonna work. but perhaps we can just use a different move group

--- src/planning.cpp
+++ src/planning.cpp
@@ -7,6 +7,7 @@
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 #include <ros/ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 //    std::vector<double> out;
 //    }
@@ -19,6 +20,8 @@
 //
 //    return {out};
 //  }
+
+double deg2rad(double deg) { return deg * M_PI / 180.0; }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "planning_demo");
@@ -45,19 +48,24 @@ int main(int argc, char **argv) {
   visual_tools.deleteAllMarkers();
 
   auto planning_scene = std::make_shared<planning_scene::PlanningScene>(robot_model);
+  robot_state::RobotState state(robot_model);
+  // NOTE q could be passed in
+  std::vector<double> q(state.getVariableCount(), 0.0);
+  planning_scene->getCurrentStateNonConst().setVariablePositions(q);
+
+  // Can we plan to keep one gripper still and move the other to a goal position?
+  state.update();  // update FK
+  auto const current_left_tool_tf = state.getGlobalLinkTransform("left_tool");
+  tf2::Vector3 const current_left_tool_position(current_left_tool_tf.translation().x(),
+                                                current_left_tool_tf.translation().y(),
+                                                current_left_tool_tf.translation().z());
 
   // Solve IK
   auto opts = std::make_unique<bio_ik::BioIKKinematicsQueryOptions>();
   opts->replace = true;                       // needed to replace the default goals!!!
   opts->return_approximate_solution = false;  // optional
-  opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>("left_tool", tf2::Vector3(0.8, 0.2, 0.8)));
+  opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>("left_tool", current_left_tool_position));
   opts->goals.emplace_back(std::make_unique<bio_ik::PositionGoal>("right_tool", tf2::Vector3(0.8, -0.2, 0.7)));
-
-  robot_state::RobotState state(robot_model);
-  // NOTE q could be passed in
-  std::vector<double> q(state.getVariableCount(), 0.0);
-  planning_scene->getCurrentStateNonConst().setVariablePositions(q);
-  state.update();  // update FK
 
   const moveit::core::JointModelGroup *jmg = state.getJointModelGroup(group_name);
 
@@ -85,8 +93,8 @@ int main(int argc, char **argv) {
       moveit_msgs::JointConstraint joint_constraint;
       joint_constraint.joint_name = n;
       joint_constraint.position = state.getVariablePosition(n);
-      joint_constraint.tolerance_above = 0.01;
-      joint_constraint.tolerance_below = 0.01;
+      joint_constraint.tolerance_above = deg2rad(0.5);
+      joint_constraint.tolerance_below = deg2rad(0.5);
       joint_constraint.weight = 1.0;
       joints_goal_constraint.joint_constraints.push_back(joint_constraint);
     }
@@ -94,7 +102,29 @@ int main(int argc, char **argv) {
     req.goal_constraints.push_back(joints_goal_constraint);
   }
 
-  ROS_INFO_STREAM("Planning with " << req.goal_constraints.size() << " goals");
+  // keep the left tool where it currently is
+  moveit_msgs::PositionConstraint left_tool_path_constraint;
+  left_tool_path_constraint.link_name = "left_tool";
+  left_tool_path_constraint.header.frame_id = "robot_root";
+  left_tool_path_constraint.weight = 1.0;
+  // convert the Transform to a Pose message
+  geometry_msgs::Pose current_left_tool_pose;
+  // TODO: we must already have a function for this???
+  current_left_tool_pose.position.x = current_left_tool_tf.translation().x();
+  current_left_tool_pose.position.y = current_left_tool_tf.translation().y();
+  current_left_tool_pose.position.z = current_left_tool_tf.translation().z();
+  current_left_tool_pose.orientation.x = current_left_tool_tf.rotation()(0);
+  current_left_tool_pose.orientation.y = current_left_tool_tf.rotation()(1);
+  current_left_tool_pose.orientation.z = current_left_tool_tf.rotation()(2);
+  current_left_tool_pose.orientation.w = current_left_tool_tf.rotation()(3);
+  left_tool_path_constraint.constraint_region.primitive_poses.push_back(current_left_tool_pose);
+  shape_msgs::SolidPrimitive path_pos_constraint_region;
+  path_pos_constraint_region.type = path_pos_constraint_region.SPHERE;
+  path_pos_constraint_region.dimensions.push_back(1.0);
+  left_tool_path_constraint.constraint_region.primitives.push_back(path_pos_constraint_region);
+  req.path_constraints.position_constraints.push_back(left_tool_path_constraint);
+
+  ROS_INFO_STREAM("Planning with " << req.goal_constraints.size() << " goal constraints");
 
   planning_interface::MotionPlanResponse res;
   req.allowed_planning_time = 30.0;
